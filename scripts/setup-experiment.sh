263587a41648fb6bdfafea12d9649ca4a4612eb2
Thomas Huetter <thomas.huetter@stud.sbg.ac.at>
2021-12-27 11:07:22 +0100

    added scripts to setup and perform the overall experiment

---
+++ scripts/setup-experiment.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# The MIT License (MIT)
+# Copyright (c) 2021 Thomas Hütter.
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# Setup the experimental evaluation performed by Huetter et al. (2022).
+
+# Fetch the timing measurement library, and the algorithms repository.
+mkdir -p external/
+cd external/
+git clone https://frosch.cosy.sbg.ac.at/wmann/common-code.git
+git clone https://github.com/DatabaseGroup/tree-similarity.git
+
+# Fetch the timing measurement library, and the algorithms repository.
+git clone https://github.com/DatabaseGroup/jedi-datasets.git
+cd jedi-datasets
+sh scripts/download-prepare.sh
+cd ../..
+
+# Build the experimental environment.
+mkdir -p build/
+cd build
+cmake ..
+make
+cd ..
