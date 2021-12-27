6049b93d809e92db87a952694be7b8f021bfa6b7
Thomas Huetter <thomas.huetter@stud.sbg.ac.at>
2021-12-27 11:09:35 +0100

    fixed name

--- scripts/perform-experiment.sh
+++ scripts/perform-experiment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # The MIT License (MIT)
-# Copyright (c) 2021 Thomas Hütter.
+# Copyright (c) 2021 Thomas Huetter.
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
